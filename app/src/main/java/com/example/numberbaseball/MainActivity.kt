db12e1ac7c28dc8de446d54b5143b2c535d38a52
jmc98 <jmc0820@naver.com>
2024-06-02 18:08:28 +0900

    Refactor(MainActivity.kt): 수정 checkPredictingNumber 함수
    예측한 숫자가 범위 내에 있는지 확인

--- app/src/main/java/com/example/numberbaseball/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseball/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.numberbaseball
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +12,16 @@ class MainActivity : AppCompatActivity() {
 	private val RANGE = (1..9)
 	//예측한 숫자
 	lateinit var predictingNumber:EditText
+	lateinit var checkButton: Button
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 		predictingNumber = findViewById(R.id.input)
+		checkButton = findViewById(R.id.check)
+		checkButton.setOnClickListener {
+			Log.d("checkButton", predictingNumber.text.toString().toInt().toString())
+		}
+
 	}
 	//숫자 생성
 	fun makeNumber():Int{
@@ -32,14 +41,19 @@ class MainActivity : AppCompatActivity() {
 
 	fun run(){
 		while (true){
-
+			var predictingNumber = checkPredictingNumber(predictingNumber)
 		}
 	}
 	//예측한 숫자가 유효한지 확인
-	fun checkPredictingNumber(predictingNumber:EditText):Int{
+	fun checkPredictingNumber(predictingNumber:EditText):Boolean{
 		var predictingNumber = predictingNumber.text.toString().toInt()
-		if (Math.pow(10.0, predictingNumber.toDouble()-1)>predictingNumber) error("checkPredictingNumber")
-		return predictingNumber
+		if (predictingNumber>999 || predictingNumber<100){
+			Toast.makeText(this, "숫자를 3자리로 입력해주세요", Toast.LENGTH_SHORT).show()
+			return false
+		}
+		return true
+
+
 	}
 
 }
