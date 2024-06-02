fff5c91c928ef39460d7067696769c32803ec62a
jmc98 <jmc0820@naver.com>
2024-06-02 17:37:45 +0900

    Feat(MainActivity.kt): 추가 makeNumber 함수
    랜덤 숫자 함수 추가

--- app/src/main/java/com/example/numberbaseball/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseball/MainActivity.kt
@@ -7,9 +7,27 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+	//숫자 범위
+	private val RANGE = (1..9)
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
+	}
+	//숫자 생성
+	fun makeNumber():Int{
+		var first:Int
+		var second:Int
+		var third:Int
+
+		first = RANGE.random()
+		do {
+			second = RANGE.random()
+		} while (first == second)
+		do {
+			third = RANGE.random()
+		} while (first == third || second == third)
+		return first * 100 + second * 10 + third
 	}
 }
