63df150bb42cd23c9e53f849080a5218777ab039
jmc98 <jmc0820@naver.com>
2024-06-02 17:49:45 +0900

    Feat(MainActivity.kt): 추가 checkPredictingNumber 함수
    예측한 숫자가 유효한지 확인

--- app/src/main/java/com/example/numberbaseball/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseball/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.numberbaseball
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 	//숫자 범위
 	private val RANGE = (1..9)
-
+	//예측한 숫자
+	lateinit var predictingNumber:EditText
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-
+		predictingNumber = findViewById(R.id.input)
 	}
 	//숫자 생성
 	fun makeNumber():Int{
@@ -30,4 +29,17 @@ class MainActivity : AppCompatActivity() {
 		} while (first == third || second == third)
 		return first * 100 + second * 10 + third
 	}
+
+	fun run(){
+		while (true){
+
+		}
+	}
+	//예측한 숫자가 유효한지 확인
+	fun checkPredictingNumber(predictingNumber:EditText):Int{
+		var predictingNumber = predictingNumber.text.toString().toInt()
+		if (Math.pow(10.0, predictingNumber.toDouble()-1)>predictingNumber) error("checkPredictingNumber")
+		return predictingNumber
+	}
+
 }
